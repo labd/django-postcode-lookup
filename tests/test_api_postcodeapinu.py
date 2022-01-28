7e2696795c6b7848e5f5e18799e1c06560c5f89e
vincent <v.dejong@labdigital.nl>
2022-01-28 17:12:02 +0100

    improve postcodeapinu test

--- tests/test_api_postcodeapinu.py
+++ tests/test_api_postcodeapinu.py
@@ -18,8 +18,8 @@ def test_api_postcodeapinu_valid(settings):
     }
 
     params = {
-        'postcode': '3625 KL',
-        'number': '14',
+        'postcode': '6545CA',
+        'number': '29',
     }
     request = rf.post('/', data=params, format='json')
 
@@ -27,21 +27,30 @@ def test_api_postcodeapinu_valid(settings):
     view = views.PostcodeLookupView.as_view()
 
     with requests_mock.Mocker() as m:
-        response = {
-            'street': 'Kanaalweg',
-            'number': '14',
-            'postcode': '3625 KL',
-            'city': 'Utrecht',
+        json_response = {
+            "postcode": "6545CA",
+            "number": 29,
+            "street": "Binderskampweg",
+            "city": "Nijmegen",
+            "municipality": "Nijmegen",
+            "province": "Gelderland",
+            "location": {
+                "type": "Point",
+                "coordinates": [
+                    5.858910083770752,
+                    51.84376540294041
+                ]
+            }
         }
         m.get(
-            "https://sandbox.postcodeapi.nu/v3/lookup/3625KL/14",
-            json=response)
+            "https://sandbox.postcodeapi.nu/v3/lookup/6545CA/29",
+            json=json_response)
 
         response = view(request)
         assert response.status_code == 200, response.rendered_content
         assert response.data == {
-            'street': 'Kanaalweg',
-            'number': '14',
-            'postcode': '3625 KL',
-            'city': 'Utrecht',
+            'street': 'Binderskampweg',
+            'number': 29,
+            'postcode': '6545 CA',
+            'city': 'Nijmegen',
         }
