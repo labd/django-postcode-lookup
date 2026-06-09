be95f2cbde3278f910833d65bb5a948b00f35d9a
Mike Dingjan <m.dingjan@labdigital.nl>
2026-06-09 14:25:54 +0200

    chore: update version specifier

--- src/django_postcode_lookup/__init__.py
+++ src/django_postcode_lookup/__init__.py
@@ -2,6 +2,6 @@ from django_postcode_lookup.utils.version import get_version
 
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 0, 1, "final", 1)
+VERSION = (2, 0, 0, "alpha", 0)
 
 __version__ = get_version(VERSION)
